5ece0eff5dd79c0d933760d580d683af34c4269c
Julie Perilla Garcia <garciaj4@mskcc.org>
2025-11-28 12:50:09 +0000

    add token embeddings

--- llm_tests.py
+++ llm_tests.py
@@ -52,3 +52,30 @@ print("First batch input IDs:", first_batch)
 print("\n")
 second_batch = next(data_iter)
 print("Second batch input IDs:", second_batch)
+
+# Convert tokens to embeddings - small example
+import torch
+vocab_size = 6
+output_dim = 3
+torch.manual_seed(123)
+embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
+#rint(embedding_layer.weight)
+#print(embedding_layer(torch.tensor([3])))
+
+# Convert tokens to embeddings - larger example
+vocab_size = 50257
+output_dim = 256
+token_embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
+max_length = 4
+dataloader = create_dataloader(raw_text, batch_size=8, max_length=max_length, stride=max_length, shuffle=False)
+data_iter = iter(dataloader)
+inputs, targets = next(data_iter)
+print("Input IDs shape:", inputs.shape)
+token_embeddings = token_embedding_layer(inputs)
+print("Token embeddings shape:", token_embeddings.shape)
+context_length = max_length
+pos_embedding_layer = torch.nn.Embedding(context_length, output_dim)
+pos_embeddings = pos_embedding_layer(torch.arange(context_length))
+print(pos_embeddings.shape)
+input_embeddings = token_embeddings + pos_embeddings
+print(input_embeddings.shape)
